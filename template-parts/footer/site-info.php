e84932366b409fac485a7bc5db6ff3e1873ed318
Simone Iannacone <s.iannacone@ecom-boost.de>
2019-11-18 17:25:57 +0100

    general credits

--- template-parts/footer/site-info.php
+++ template-parts/footer/site-info.php
@@ -2,6 +2,6 @@
 	<div class="col-xs-12">
 		<?php the_privacy_policy_link(); ?>
 		<span> | </span>
-		<span class="site-author">&copy; ecom-boost <?php echo date('Y'); ?></span>
+		<span class="site-author">&copy; credits <?php echo date('Y'); ?></span>
 	</div>
 </div>
