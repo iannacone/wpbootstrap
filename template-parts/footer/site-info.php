8f9a675dfca5964c8bd87b3cf2215a8bf5530a75
Simone Iannacone <s.iannacone@ecom-boost.de>
2019-12-11 15:09:53 +0100

    bootstrap 4 migration
    - fixed menu
    - updated wp-bootstrap-navwalker
    - setted up a generic footer
    
    Co-Authored-By: Simon Iannacone <iannacone@users.noreply.github.com>

--- template-parts/footer/site-info.php
+++ template-parts/footer/site-info.php
@@ -1,5 +1,5 @@
 <div class="site-info">
-	<?php the_privacy_policy_link(); ?>
-	<span> | </span>
-	<span class="site-author">&copy; credits <?php echo date('Y'); ?></span>
+	<?php /* <?php the_privacy_policy_link(); ?>
+	<span> | </span> */ ?>
+	<span class="site-author">&copy; <?php bloginfo('name'); ?> <?php echo date('Y'); ?></span>
 </div>
--- template-parts/navigations/primary-menu.php
+++ template-parts/navigations/primary-menu.php
@@ -7,27 +7,29 @@
 
 if (has_nav_menu('primary-menu')) {
 	?>
-	<nav id="site-navigation" class="main-navigation navbar navbar-default" role="navigation" aria-label="<?php esc_attr_e('Top Menu'); ?>">
+	<nav id="site-navigation" class="main-navigation navbar navbar-expand-md navbar-light" role="navigation" aria-label="<?php esc_attr_e('Top Menu'); ?>">
 		<div class="container">
-			<div class="navbar-header">
-				<?php /*  mobile  */ ?>
-				<button type="button" class="menu-toggle navbar-toggle collapsed" data-toggle="collapse" data-target="#primary-menu" aria-controls="primary-menu" aria-expanded="false" aria-label="<?php esc_attr_e('Toggle navigation'); ?>">
-					<span class="sr-only"><?php esc_html_e('Toggle navigation'); ?></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<?php get_template_part('template-parts/header/site', 'branding'); ?>
-			</div>
+			
+			<?php get_template_part('template-parts/header/site', 'branding'); ?>
+			
+			<?php /*  mobile  */ ?>
+			<button type="button" class="menu-toggle navbar-toggler collapsed" data-toggle="collapse" data-target="#primary-menu" aria-controls="primary-menu" aria-expanded="false" aria-label="<?php esc_attr_e('Toggle navigation'); ?>">
+				<span class="sr-only"><?php esc_html_e('Toggle navigation'); ?></span>
+				<span class="navbar-toggler-icon"></span>
+				<?php /* <span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span> */ ?>
+			</button>
+			
 			<div id="primary-menu" class="collapse navbar-collapse">
-				<?php wp_nav_menu(array(
-					// 'menu_id' => 'primary-menu',
+				<?php wp_nav_menu([
 					'theme_location' => 'primary-menu',
 					'container' => false,
 					'menu_class' => 'nav navbar-nav',
 					'walker' => new \WP_Bootstrap_Navwalker(),
-				)); ?>
+				]); ?>
 			</div>
+			
 		</div>
 	</nav>
 	<div id="primary-menu-replacement"></div>
