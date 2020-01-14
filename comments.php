8fb4d34c86ac749088838f23c8c06a9b537440bf
Simone Iannacone <s.iannacone@ecom-boost.de>
2020-01-14 15:59:32 +0100

    typo
    Co-Authored-By: Simon Iannacone <iannacone@users.noreply.github.com>

--- comments.php
+++ comments.php
@@ -75,11 +75,10 @@ if (post_password_required()) {
 
 
 	// If comments are closed and there are comments, let's leave a little note, shall we?
-	if (!comments_open() && get_comments_number() && post_type_supports(get_post_type(), 'comments')) { ?>
+	if (!comments_open() && get_comments_number() && post_type_supports(get_post_type(), 'comments')) {
+		?>
 		<p class="no-comments"><?php esc_html_e('Comments are closed.'); ?></p>
-	<?php }
-	*/
-	?>
+	<?php } ?>
 	
 	<div>
 		<?php
