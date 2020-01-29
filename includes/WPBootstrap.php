28c1bbfba9ad6cb6d7efab52b34b290d4b987533
Simone Iannacone <s.iannacone@ecom-boost.de>
2020-01-29 20:01:33 +0100

    improved theme supports
    Co-Authored-By: Simon Iannacone <iannacone@users.noreply.github.com>

--- includes/WPBootstrap.php
+++ includes/WPBootstrap.php
@@ -103,11 +103,42 @@ class WPBootstrap {
 				// 	'site-description',
 				// ],
 			],
-			'custom-background',
-			'custom-header',
+			'custom-background' => [
+				'default-image'          => '',
+				'default-preset'         => 'default', // 'default', 'fill', 'fit', 'repeat', 'custom'
+				'default-position-x'     => 'left',    // 'left', 'center', 'right'
+				'default-position-y'     => 'top',     // 'top', 'center', 'bottom'
+				'default-size'           => 'auto',    // 'auto', 'contain', 'cover'
+				'default-repeat'         => 'repeat',  // 'repeat-x', 'repeat-y', 'repeat', 'no-repeat'
+				'default-attachment'     => 'scroll',  // 'scroll', 'fixed'
+				'default-color'          => '',
+				'wp-head-callback'       => '_custom_background_cb',
+				'admin-head-callback'    => '',
+				'admin-preview-callback' => '',
+			],
+			'custom-header' => [
+				'default-image'          => '',
+				'random-default'         => false,
+				'width'                  => 0,
+				'height'                 => 0,
+				'flex-height'            => false,
+				'flex-width'             => false,
+				'default-text-color'     => '',
+				'header-text'            => true,
+				'uploads'                => true,
+				'wp-head-callback'       => '',
+				'admin-head-callback'    => '',
+				'admin-preview-callback' => '',
+				'video'                  => false,
+				'video-active-callback'  => 'is_front_page',
+			],
 			'automatic-feed-links',
 			'title-tag',
-			'post-thumbnails',
+			'post-thumbnails' => [
+				'post',
+				'page',
+				'id',
+			],
 			'html5' => [
 				'search-form',
 				'comment-form',
