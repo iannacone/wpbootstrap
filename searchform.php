b69c9f307caa7eb16036af13a28b080c9ad46a75
Simone Iannacone <iannaconda_06@yahoo.it>
2020-03-05 17:18:20 +0100

    little fixes
    - added "placeholders" to the scss structure
    - improved the search form
    
    Co-Authored-By: Simon Iannacone <iannacone@users.noreply.github.com>

--- searchform.php
+++ searchform.php
@@ -6,17 +6,21 @@
  */
  
  /* http://adrianroselli.com/2015/08/where-to-put-your-search-role.html */
- 
-$unique_id = esc_attr(uniqid('search-form-'));
-?>
 
-<div role="search">
+
+
+$unique_id = esc_attr(uniqid('search-form-'));
+
+?>
+<div class="search-form" role="search">
 	<form method="get" class="search-form" action="<?php echo esc_url(home_url('/')); ?>">
 		<label for="<?php echo $unique_id; ?>">
 			<span class="screen-reader-text"><?php echo _x('Search for:', 'label') ?></span>
 			<input type="search" id="<?php echo $unique_id; ?>" class="search-field" placeholder="<?php echo esc_attr_x('Search …', 'placeholder') ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x('Search for:', 'label') ?>" />
-			<i class="fas fa-search">&nbsp;</i>
 		</label>
-		<input type="submit" class="search-submit" value="<?php echo esc_attr_x('Search', 'submit button') ?>" />
+		<button type="submit" class="search-submit">
+			<span class="screen-reader-text"><?php esc_html_e('Search', 'submit button') ?></span>
+			<i class="fas fa-search">&nbsp;</i>
+		</button>
 	</form>
 </div>
