48527757cd3950c6a76bbea131605aac17a3d014
Simone Iannacone <iannaconda_06@yahoo.it>
2020-06-04 17:07:13 +0200

    composer autoloader

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * functions and defines
+ * starter
  *
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  *
@@ -14,17 +14,8 @@ namespace WPBootstrap;
 
 
 
-define('I18N_TEXTDOMAIN', 'wp-bootstrap');
-define('WPBOOTSTRAP_FOOTER_COLUMNS', 4);
-define('WPBOOTSTRAP_VERSION', '4.5');
-define('WPBS_CLEARCACHE', 'clearCache');
-define('WPBS_CSSCACHE', 'style.css');
-define('FONT_SIZE_BASE', 16);
-define('WP_SCSS_ALWAYS_RECOMPILE', \WP_DEBUG);
-
-
-
-require_once('Autoload.php');
+require_once('defines.php');
+require(WPBOOTSTRAP_VENDOR_ABS . '/autoload.php');
 
 
 
